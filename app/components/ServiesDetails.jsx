ea4385050284528e00e7872a69d475611b1d645a
antor94 <antorremeni@gmail.com>
2025-11-13 12:40:11 +0600

    chnage

--- app/components/ServiesDetails.jsx
+++ app/components/ServiesDetails.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import ServicesCommon from './common/ServicesCommon'
 
+import serviceImg from '../../public/iamges/servicesImg.png'
+import SerIcon from '../../public/iamges/SerIcon.png'
+
+
+
 const ServiesDetails = () => {
   return (
     <>
     
-    <section id='services'>
+    <section id='services' className='py-[120px]'>
         <div className="container">
-            <div>
 
-            <ServicesCommon />
+
+            <div className='flex flex-col gap-[120px]'>
+
+            <ServicesCommon serH2={'Residential Construction'} serP={'We create modern homes that prioritize comfort, safety, and sustainability. From single-family homes to multi-unit developments, our residential projects are built to exceed expectations and stand the test of time.'} serIcon={SerIcon} serImg={serviceImg}  />
+            <ServicesCommon flexdirection={'flex-row-reverse'}  serH2={'Commercial Projects'} serP={'Business spaces designed for efficiency and long-term value. We understand that commercial buildings need to balance functionality, aesthetics, and cost-effectiveness to support your business goals.'} serIcon={SerIcon} serImg={serviceImg} />
+            <ServicesCommon  serH2={'Industrial Development'} serP={'Heavy-duty, performance-driven structures for industry leaders. Our industrial projects are engineered to handle demanding operations while maintaining safety and environmental standards.  '} serIcon={SerIcon} serImg={serviceImg} />
+            <ServicesCommon flexdirection={'flex-row-reverse'}  serH2={'Renovation & Remodeling'} serP={'Transforming existing spaces with new life and function. Whether updating outdated structures or reimagining spaces for new purposes, we bring fresh vision to every renovation project.'} serIcon={SerIcon} serImg={serviceImg} />
+            <ServicesCommon  serH2={'Design & Build'} serP={'One-stop solution for planning, engineering, and construction. Our integrated approach streamlines the process, reduces costs, and ensures seamless coordination from concept to completion.'} serIcon={SerIcon} serImg={serviceImg} />
 
             </div>
         </div>
--- app/components/common/ServicesCommon.jsx
+++ app/components/common/ServicesCommon.jsx
@@ -2,31 +2,28 @@ import Image from 'next/image';
 import React from 'react'
 import { IoMdCheckmarkCircleOutline } from "react-icons/io";
 
-import serviceImg from '../../../public/iamges/servicesImg.png'
-import SerIcon from '../../../public/iamges/SerIcon.png'
 
-
-const ServicesCommon = () => {
+const ServicesCommon = ({serH2,serP,serImg,serIcon ,flexdirection }) => {
   return (
     <>
     
 
-    <div className='w-full h-[380px] flex items-center'>
+    <div className={`w-full h-[380px] flex justify-center gap-[48px] items-center ${flexdirection}`}>
 
         {/* ---------------- left-side content */}
 
         <div>
-            <div className='w-[52px] h-[52px] rounded-[16px] flex justify-center items-center bg-[#2e7d321f]'><Image src={SerIcon} alt='SerIcon' width={32} height={32} /></div>
+            <div className='w-[52px] h-[52px] rounded-[16px] flex justify-center items-center bg-[#2e7d321f]'><Image src={serIcon} alt='SerIcon' width={32} height={32} /></div>
 
-            <h2 className='text-[32px] mt-[32px] mb-[16px] font-semibold font-main text-[#09231E]'>Residential Construction</h2>
-            <div className='w-[636px]'><h4 className='text-[16px] font-normal font-main text-[#4A5565]'>We create modern homes that prioritize comfort, safety, and sustainability. From single-family homes to multi-unit developments, our residential projects are built to exceed expectations and stand the test of time.</h4></div>
+            <h2 className='text-[32px] mt-[32px] mb-[16px] font-semibold font-main text-[#09231E]'>{serH2}</h2>
+            <div className='w-[636px]'><h4 className='text-[16px] font-normal font-main text-[#4A5565]'>{serP}</h4></div>
 
-            <div>
+            <div className='mt-[24px]'>
                 <ul className='flex flex-col gap-[12px]'>
-                    <li className='text-[16px] font-normal font-main text-[#4A5565] flex items-center gap-[12px]'><IoMdCheckmarkCircleOutline className='text-[20px] text-[#2E7D32]' />Custom Home Design</li>
-                    <li className='text-[16px] font-normal font-main text-[#4A5565] flex items-center gap-[12px]'><IoMdCheckmarkCircleOutline className='text-[20px] text-[#2E7D32]' />Luxury Apartments</li>
-                    <li className='text-[16px] font-normal font-main text-[#4A5565] flex items-center gap-[12px]'><IoMdCheckmarkCircleOutline className='text-[20px] text-[#2E7D32]' />Housing Complexes</li>
-                    <li className='text-[16px] font-normal font-main text-[#4A5565] flex items-center gap-[12px]'><IoMdCheckmarkCircleOutline className='text-[20px] text-[#2E7D32]' />Green Building Solutions</li>
+                    <li className='text-[16px] font-normal font-main text-[#4A5565] flex items-center gap-[12px]'><IoMdCheckmarkCircleOutline className='text-[20px] text-[#2E7D32]' /></li>
+                    <li className='text-[16px] font-normal font-main text-[#4A5565] flex items-center gap-[12px]'><IoMdCheckmarkCircleOutline className='text-[20px] text-[#2E7D32]' /></li>
+                    <li className='text-[16px] font-normal font-main text-[#4A5565] flex items-center gap-[12px]'><IoMdCheckmarkCircleOutline className='text-[20px] text-[#2E7D32]' /></li>
+                    <li className='text-[16px] font-normal font-main text-[#4A5565] flex items-center gap-[12px]'><IoMdCheckmarkCircleOutline className='text-[20px] text-[#2E7D32]' /></li>
                 </ul>
             </div>
         </div>
@@ -34,7 +31,7 @@ const ServicesCommon = () => {
 
         {/* ----------------- right-side Immge */}
 
-        <Image src={serviceImg} width={636} height={380} alt='img' />
+        <Image src={serImg} width={636} height={380} alt='img' />
         
     </div>
     
