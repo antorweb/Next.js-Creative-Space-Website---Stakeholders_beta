68874fa02a9af08969246aea68a84864531eed88
antor94 <antorremeni@gmail.com>
2025-11-13 11:38:07 +0600

    chnage

---
+++ app/components/Banner.jsx
@@ -0,0 +1,78 @@
+import React from "react";
+
+import { HiArrowRight } from "react-icons/hi";
+
+
+const Banner = () => {
+  return (
+    <>
+      <section id="AntorBanner" className=" ">
+        <div id="bannerContain" className=" py-20 ">
+          <div className="container">
+
+
+            {/* --------------- */}
+
+
+            <div className="text-center flex flex-col  justify-center items-center">
+
+              <div className="w-[250px]  bg-[#ffffff36] flex items-center justify-center gap-2.5 py-3 rounded-full ">
+                {" "}
+                <span className="w-4 h-4 inline-block bg-[#ffffff50] drop-shadow-2xl rounded-full "></span>{" "}
+                <h2 className="text-[14px] font-medium font-main  text-white">
+                  Your Vision, Our Expertise
+                </h2>{" "}
+              </div>
+
+              <h1 className="text-[64px] font-bold font-main my-[16px] text-white">Building the Future, Today</h1>
+              <div className="w-[683px] text-center">
+                <h4 className="text-[20px] font-normal font-main text-[#F3F4F6]">
+                  Transform your construction dreams into reality with
+                  sustainable practices and innovative design excellence.
+                </h4>
+              </div>
+
+              <div className="flex items-center gap-4 mt-10">
+                <button className="w-[203px] text-[14px] font-medium font-main text-white flex justify-center items-center gap-[10px] bg-[#2E7D32] py-[16px] rounded-full hover:bg-white hover:border-[#2E7D32] hover:text-[#2E7D32] duration-400 ">
+                  Discover Our Story <HiArrowRight />
+                </button>
+                <button className="w-[154px] text-[14px] font-medium font-main text-white flex justify-center items-center gap-[10px] bg-[#2E7D32] py-[16px] rounded-full hover:bg-white hover:border-[#2E7D32] hover:text-[#2E7D32] duration-400">Get Started <HiArrowRight /></button>
+              </div>
+
+
+
+              <div>
+
+                     <div className="w-[370px]  bg-[#ffffff36] flex items-center justify-center gap-2.5 py-3 rounded-full ">
+                {" "}
+                <span className="w-[48px] h-[48px] inline-block bg-[#ffffff50] drop-shadow-2xl rounded-full "></span>{" "}
+
+                <div>
+                    <p>Contact Us</p>
+                    <h5>+880 1234 567 890</h5>
+                </div>
+           
+              </div>
+
+
+                   <div className="w-[370px]  bg-[#ffffff36] flex items-center justify-center gap-2.5 py-3 rounded-full ">
+                {" "}
+                <span className="w-4 h-4 inline-block bg-[#ffffff50] drop-shadow-2xl rounded-full "></span>{" "}
+                <h2 className="text-[14px] font-medium font-main  text-white">
+                  Your Vision, Our Expertise
+                </h2>{" "}
+              </div>
+
+              </div>
+
+
+
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Banner;
---
+++ app/components/Navber.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import mainLogo from "../../public/iamges/main-logo.png";
+import Link from "next/link";
+import Image from "next/image";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav id="Antor-Navbar" className="py-7">
+        <div className="container">
+          <div className="flex justify-between items-center">
+            {/* ------------ main-logo */}
+            <Link href={"/"}>
+              <Image src={mainLogo} width={119} alt="main-logo" />
+            </Link>
+
+            {/* ------------------- nav-content */}
+
+            <div className="flex items-center gap-12">
+              <div>
+                <ul className="flex gap-8 items-center">
+                  <li>
+                    <Link
+                      href={"/"}
+                      className="text-[16px] font-normal font-main text-primary hover:text-[#2E7D32] duration-300"
+                    >
+                      Home
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      href={"/"}
+                      className="text-[16px] font-normal font-main text-primary hover:text-[#2E7D32] duration-300"
+                    >
+                      About
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      href={"/"}
+                      className="text-[16px] font-normal font-main text-primary hover:text-[#2E7D32] duration-300"
+                    >
+                      Services
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      href={"/"}
+                      className="text-[16px] font-normal font-main text-primary hover:text-[#2E7D32] duration-300"
+                    >
+                      Projects
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      href={"/"}
+                      className="text-[16px] font-normal font-main text-primary hover:text-[#2E7D32] duration-300"
+                    >
+                      Contact
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+
+              <button className="w-[129px] bg-[#2E7D32] text-[14px] font-medium font-main text-white py-2 rounded-full">
+                Get a Quote
+              </button>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
