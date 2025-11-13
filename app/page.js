95bfa062f551198da7060981b779994c9b33147e
antor94 <antorremeni@gmail.com>
2025-11-13 11:42:30 +0600

    chnage

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Banner from './components/Banner'
 
 const page = () => {
   return (
@@ -6,7 +7,7 @@ const page = () => {
     
     
     
-    
+    <Banner />
     
     
     
